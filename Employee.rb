90e1799b6acefed9ec92b86649e6fd968e4f6815
Aalekh Modgil <aalekhmodgil8@gmail.com>
2024-12-04 20:50:05 +0530

    [Aalekh] ADD : Adding UC7

--- Employee.rb
+++ Employee.rb
@@ -1,23 +1,47 @@
-part_time_hours = 4
-full_time_hours = 8
-per_hour_wage = 20
-max_working_hours = 100
-monthly_wage = 0
+class Employee
 
-for i in 1..20
-  rng = Random.new
-  type_of_work = rng.rand(0..2)
-  if max_working_hours <=0
-    break
-  elsif type_of_work == 0
-    monthly_wage += 0
-  elsif type_of_work == 1
-    max_working_hours -= 4
-    monthly_wage += (part_time_hours*per_hour_wage)
-  elsif type_of_work == 2
-    max_working_hours -= 8
-    monthly_wage += (full_time_hours*per_hour_wage)
+  @@daily_wage = 20
+  @@part_time_hours = 4
+  @@full_time_hours = 8
+
+
+  def initialize(type_of_work)
+    @type_of_work = type_of_work
+    @monthly_salary = 0
   end
+
+  def daily_wage()
+    if @type_of_work == 0
+      return 0
+    elsif @type_of_work == 1
+      return @@part_time_hours * @@daily_wage
+    elsif @type_of_work == 2
+      return @@full_time_hours * @@daily_wage
+    end
+  end
+
+  def monthly_wage()
+    for i in 1..20
+      rng = Random.new
+      type = rng.rand(0..2)
+      if type == 0
+        @monthly_salary += 0
+      elsif type == 1
+        @monthly_salary += @@part_time_hours * @@daily_wage
+      elsif type == 2
+        @monthly_salary += @@full_time_hours * @@daily_wage
+      end
+    end
+    return @monthly_salary
+  end
+
 end
 
-puts "Monthly wage of employee :- #{monthly_wage}"
+rng = Random.new
+
+type_of_work = rng.rand(0..2)
+
+employee1 = Employee.new(type_of_work)
+
+puts "Daily wage :-  #{employee1.daily_wage}"
+puts "Monthly wage :- #{employee1.monthly_wage}"
