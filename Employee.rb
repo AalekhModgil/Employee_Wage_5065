a79e0061c8fcd6ee1c25d0832f6ff82f881c92b2
Aalekh Modgil <aalekhmodgil8@gmail.com>
2024-12-04 20:36:57 +0530

    [Aalekh] ADD : Adding UC3

--- Employee.rb
+++ Employee.rb
@@ -1,26 +1,16 @@
+per_hour_wage = 20
 part_time_hours = 4
 full_time_hours = 8
-per_hour_wage = 20
-
-# Randomly check it is no_time = 0 , part_time = 1 , full_time = 2
 
 rng = Random.new
-type_of_work = rng.rand(0..2)
-work_hours = nil
+work_type = rng.rand(0..2)
 
-if type_of_work == 0
-    work_hours = 0
-elsif type_of_work == 1
-    work_hours = part_time_hours
-elsif type_of_work == 2
-    work_hours = full_time_hours
-else
-    work_hours = 0
+if work_type == 0
+  puts "Daily wage :- #{0}"
+elsif work_type == 1
+  puts "Daily wage :- #{4*per_hour_wage}"
+elsif work_type == 2
+  puts "Daily Wage :- #{8*per_hour_wage}"
 end
-
-
-daily_wage = work_hours*per_hour_wage
-
-puts "Daily wage of an employee is :- #{daily_wage}"
 
 
